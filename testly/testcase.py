2bd7825147d7d86038c776994894de25b957d34d
Josh Lee <jleedev@gmail.com>
2010-06-20 17:44:39 -0400

    Made TestModule an iterable and tested it.

--- testly/testcase.py
+++ testly/testcase.py
@@ -14,11 +14,15 @@ class TestModule(object):
         self.name = module
         self.module = __import__(self.name, fromlist=True)
 
-    def __call__(self):
+    def __iter__(self):
         for item in dir(self.module):
             if not test_regex.match(item):
                 continue
-            result = getattr(self.module, item)()
+            yield getattr(self.module, item)
+
+    def __call__(self):
+        for item in self:
+            result = item()
             if isinstance(result, types.GeneratorType):
                 # Generator test
                 for func, args in result:
--- tests/test_finds_tests.py
+++ tests/test_finds_tests.py
@@ -1,7 +1,13 @@
 from testly.finder import Finder
+from testly.testcase import TestModule
 
 
 def test_finds_modules_in_cwd():
     finder = Finder()
     lst = [test.name for test in finder]
     assert lst == ['tests.test_finds_tests'], "Got %s" % lst
+
+def test_finds_tests():
+    module = TestModule('.', './tests', 'test_finds_tests.py')
+    assert set(module) == \
+        set([test_finds_modules_in_cwd, test_finds_tests])
